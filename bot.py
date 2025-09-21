1b332db555aee202e08e3abd5cfb52f9173b62d8
Lasve21 <diego.bonfiglioli@gmail.com>
2025-09-21 23:37:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,14 @@ import telebot
 from flask import Flask, request
 
 TOKEN = os.environ.get("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, threaded=False)  # <-- aggiunto threaded=False
 app = Flask(__name__)
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
     json_str = request.stream.read().decode("UTF-8")
-    print("DEBUG: ricevuto POST:", json_str)  # <-- aggiungi questa riga
-    try:
-        update = telebot.types.Update.de_json(json_str)
-        bot.process_new_updates([update])
-    except Exception as e:
-        print("ERRORE:", e)  # <-- stampa eventuali errori
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
     return "!", 200
 
 @app.route("/")
